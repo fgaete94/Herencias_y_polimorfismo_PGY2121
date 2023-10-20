511e27e2017cb742fd4211f4e9af792ea9bb7dd3
vina <vina@VIAPCE02LC0306.duoc.acad>
2023-10-19 21:01:25 -0300

    PRUEBA DE COMIT

--- src/ejercicio233/Principal.java
+++ src/ejercicio233/Principal.java
@@ -48,6 +48,8 @@ public class Principal {
         System.out.println("\n*********");
         aseo2.agregarComentario("no mata los mosquitos");
         
+        //COMIT DE PRUEBA PARA PUSH
+        
     }
     
 }
